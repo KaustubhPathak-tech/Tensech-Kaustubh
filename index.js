2b95fb8ceed2b3d3ee2cf079f5c7862b91d0edd6
Kaustubh Pathak <kaustubhpathak@gmail.com>
2023-09-08 14:37:22 +0530

    finally !

--- index.js
+++ index.js
@@ -1,30 +1,47 @@
-//eslint error 6
 const express = require('express');
 const fetch = require('node-fetch');
 const app = express();
-const port = 3000; 
-
+const port = 3000;
 
 app.use(express.json());
 
 app.post('/convert', async (req, res) => {
-  try { 
-    const var1=req.body.fromcurrency;
-    const var2=req.body.tocurrency;
-    
+  try {
+    const toConvert = req.body.toConvert;
 
-    const conversionAPI = `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${var1}/${var2}.json`;
-    const response = await fetch(conversionAPI);
-    const data = await response.json();
-    
+    const conversions = [];
+
+    for (const item of toConvert) {
+      const amount = item.amount;
+      const from = (item.from).toLowerCase();
+      const toCurrencies = item.to;
+
+      for (const toCurrency of toCurrencies) {
+        const conversionAPI = `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${from}/${(toCurrency).toLowerCase()}.json`;
+        const response = await fetch(conversionAPI);
+        const data = await response.json();
+        console.log(data);
+
+        conversions.push({
+          amount: amount,
+          from: from,
+          exchangeValues: [
+            {
+              to: toCurrency,
+              value: amount*data[toCurrency.toLowerCase()],
+            },
+          ],
+        });
+      }
+    }
+
     res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify({data }));
+    res.status(200).json({ conversions });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Internal server error' });
   }
 });
-
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
